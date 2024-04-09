a67f176dd976fd871338ccbf762e4b493c86c735
tocchibo <muskos13@gmail.com>
2024-04-09 10:32:34 +0900

    dropdown動作修正

--- main.js
+++ main.js
@@ -33,29 +33,27 @@ function populateDropdowns() {
 }
 
 // スライダーを更新する関数
-function updateSliders() {
-  // 1次利用の選択値を取得
-  const primaryUse = document.getElementById('primaryUse').value;
-  // 2次利用の選択値を取得
-  const secondaryUse = document.getElementById('secondaryUse').value;
-
+function updateSliders(category) {
   // 入力データを読み込む
   const inputData = loadData();
-  // 選択された1次利用のアプリケーションを取得
-  const primaryApplication = inputData.applications.find(app => app.name === primaryUse);
-  // 選択された2次利用のアプリケーションを取得
-  const secondaryApplication = inputData.applications.find(app => app.name === secondaryUse);
 
   // 各スライダーについて
   Object.keys(settings.sliders).forEach(key => {
     const setting = settings.sliders[key];
-    if (setting.dataProperty) {
+    if (setting.dataProperty && setting.category === category) {
       // スライダーに対応するデータの値を取得
-      const value = key.startsWith('primary')
-        ? getNestedValue(primaryApplication, setting.dataProperty)
-        : key.startsWith('secondary')
-        ? getNestedValue(secondaryApplication, setting.dataProperty)
-        : getNestedValue(inputData, setting.dataProperty);
+      let application;
+      if (category === 'primary') {
+        const primaryUse = document.getElementById('primaryUse').value;
+        application = inputData.applications.find(app => app.name === primaryUse);
+      } else if (category === 'secondary') {
+        const secondaryUse = document.getElementById('secondaryUse').value;
+        application = inputData.applications.find(app => app.name === secondaryUse);
+      } else {
+        application = inputData;
+      }
+      const value = getNestedValue(application, setting.dataProperty);
+
       // スライダーの値を更新
       document.getElementById(key + '-slider').value = setting.percentage ? value * 100 : value;
       // 入力欄の値を更新
@@ -91,9 +89,9 @@ function init() {
   // 追加情報を更新
   updateInfo();
   // 1次利用のドロップダウンメニューの変更イベントにスライダーの更新を紐付け
-  document.getElementById('primaryUse').addEventListener('change', updateSliders);
+  document.getElementById('primaryUse').addEventListener('change', () => updateSliders('primary'));
   // 2次利用のドロップダウンメニューの変更イベントにスライダーの更新を紐付け
-  document.getElementById('secondaryUse').addEventListener('change', updateSliders);
+  document.getElementById('secondaryUse').addEventListener('change', () => updateSliders('secondary'));
 }
 // アプリケーションを初期化
 init();
